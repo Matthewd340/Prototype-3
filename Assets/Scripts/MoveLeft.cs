e2bd5038a568d7dc871d07933e40b5f1e40963f5
Matthewd340 <146954999+Matthewd340@users.noreply.github.com>
2024-11-04 12:33:37 -0500

    3.2 completed

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 30;
+    private PlayerController playerControllerScript;
+    public float leftBound = -15;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        //finds player game object in project and gets PlayerController script component
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerControllerScript.gameOver == false)
+        {
         transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,13 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 5;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        //name                   data type
         Physics.gravity *= gravityModifier;
     }
 
@@ -32,6 +34,11 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.tag == "Ground")
         {
             isOnGround = true;
+        }
+        else if(collision.gameObject.tag == "Obstacle")
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
         }
     }
 }
